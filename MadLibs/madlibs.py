455044bff112273f342fec6bb34ae74b852ede3c
vaikarshivani <74733564+vaikarshivani@users.noreply.github.com>
2024-03-05 15:02:31 +0530

    Create madlibs.py

---
+++ MadLibs/madlibs.py
@@ -0,0 +1,8 @@
+adj = input("Adjective: ")
+verb1 = input("Verb1: ")
+verb2 = input("Verb2: ")
+famous_person = input("Famous person: ")
+
+madlib = f"Computer programming is so {adj}! It makes me so excited all the time because " \
+        f"I love to {verb1}. Stay hydrated and {verb2} like you are {famous_person}!"
+print(madlib)
